4f5dd2cac6a321a4ea5356051d4f5df06210566d
sudheer singh <justsudheerpaliwal@gmail.com>
2017-11-29 00:09:21 +0530

    add eslint, configure react router redux, and redux-saga

--- src/index.js
+++ src/index.js
@@ -1,8 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
+import createSagaMiddleware from 'redux-saga';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
+import rootSaga from './sagas';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+
+const history = createHistory();
+
+const middlewares = [
+  sagaMiddleware,
+  routerMiddleware(history),
+];
+
+const enhancers = [
+  applyMiddleware(...middlewares),
+];
+
+const initialState = {};
+
+// If Redux DevTools Extension is installed use it, otherwise use Redux compose
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers =
+  process.env.NODE_ENV !== 'production' &&
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      shouldHotReload: false,
+    })
+    : compose;
+
+const store = createStore(
+  reducers,
+  initialState,
+  composeEnhancers(...enhancers),
+);
+
+// Extensions
+store.runSaga = sagaMiddleware.run(rootSaga);
+
+/* eslint-disable */
+const RenderApp = () => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
+);
+/* eslint-enable */
+
+ReactDOM.render(<RenderApp />, document.getElementById('app'));
 registerServiceWorker();
